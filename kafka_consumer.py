49167ec7598b5e42133f59a108f78d1719146753
raismuhammad <raismuhammad@DESKTOP-77G0G97>
2024-07-23 12:13:59 +0500

    Implementing the kafka

---
+++ kafka_consumer.py
@@ -0,0 +1,127 @@
+from kafka import KafkaConsumer 
+import json
+import os
+import time
+from datetime import timedelta, datetime
+import pyodbc
+import concurrent.futures
+from dotenv import find_dotenv, load_dotenv
+
+
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+
+# Kafka Environment variables
+KAFKA_BOOTSTRAP_SERVER = os.getenv("KAFKA_BOOTSTRAP_SERVER")
+VEHICLE_TOPIC = os.getenv("VEHICLE_TOPIC")
+GPS_TOPIC = os.getenv("GPS_TOPIC")
+TRAFFIC_TOPIC = os.getenv("TRAFFIC_TOPIC")
+WEATHER_TOPIC = os.getenv("WEATHER_TOPIC")
+EMERGENCY_TOPIC = os.getenv("EMERGENCY_TOPIC")
+
+# SQL Server database environment variables
+server = os.getenv("server")
+database = os.getenv("database")
+username = os.getenv("username")
+password = os.getenv("password")
+driver = os.getenv("driver")
+
+# Function to setup the consumer
+def topic_kafka_consumer(topic, server, group_id):
+    return KafkaConsumer(
+            topic,
+            bootstrap_servers = server,
+            auto_offset_reset="earliest",
+            enable_auto_commit = True,
+            group_id = group_id,
+            value_deserializer = lambda x: json.loads(x.decode('utf-8'))
+            )
+
+    
+# Connection with MS SQL Server
+def mssql_connection(driver, server, database, username, password):
+    try:
+        conn_str = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+        connection = pyodbc.connect(conn_str)
+        # print("Connection successful.")
+        return connection
+    except Exception as e:
+        print(f"Connection failed with error {e}")
+        return None
+
+# Generating the dynamic query according to the data in the kafka topic
+def generate_query(table, data, columns):
+    placeholders = placeholders = ", ".join(["?" for _ in data.keys()])
+    query = f"""
+        INSERT INTO {table} ({columns}) VALUES ({placeholders})
+            """
+    return query
+
+# Insert function. Insert the data into the MSSQL Server tables
+def insert_data(table, query, data):
+    try:        
+        connection = mssql_connection(driver, server, database, username, password)
+        cursor = connection.cursor()
+        cursor.execute(query, data)
+        print(f"Inserting data: {data} for table {table}, \n Successfully.....")
+        connection.commit()
+    except pyodbc.Error as e:
+        print(f"Error occured in insert data function : {e}")
+    finally:
+        cursor.close()
+        connection.close()
+
+# Function that gets the data from the kafka consumer and retrieve the required data 
+def topic_data_extraction(topics_data):
+    for message in topics_data:
+        # Extracting the topic name as a table to insert into sql db
+        table = message.topic.strip()
+        # The actual data from the consumer
+        topic_data = message.value
+        
+        # Converting the loaction to string for ease while inserting into database table
+        if 'location' in topic_data:
+            topic_data['location'] = f"{topic_data['location'][0]},{topic_data['location'][1]}"
+
+        # Extraction of the keys of json formated data as columns
+        columns = tuple(topic_data.keys())
+        columns = ', '.join(columns)
+        # Actual data as a tuple
+        data = tuple(topic_data.values())
+
+        # Insert query for inserting the data
+        query = generate_query(table, topic_data, columns)
+        insert_data(table, query, data) 
+
+
+def main():
+    print("Kafka Server Started....!")
+    # Extraction the data from each kafka topic
+    vehicle_data = topic_kafka_consumer(VEHICLE_TOPIC, KAFKA_BOOTSTRAP_SERVER, "vehicle_data")
+    gps_data = topic_kafka_consumer(GPS_TOPIC, KAFKA_BOOTSTRAP_SERVER, "gps_data")
+    traffic_camera_data = topic_kafka_consumer(TRAFFIC_TOPIC, KAFKA_BOOTSTRAP_SERVER, "traffic_camera_data")
+    weather_data = topic_kafka_consumer(WEATHER_TOPIC, KAFKA_BOOTSTRAP_SERVER, "weather_data")
+    emergency_incident_data = topic_kafka_consumer(EMERGENCY_TOPIC, KAFKA_BOOTSTRAP_SERVER, "emergency_data")
+
+    # Tasks to run in parallel
+    tasks = [
+        (vehicle_data),
+        (gps_data),
+        (traffic_camera_data),
+        (weather_data),
+        (emergency_incident_data)
+    ]
+
+    # Run the tasks in parallel
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(topic_data_extraction, data) for data in tasks]
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                future.result()  # Will raise an exception if the thread had an error
+            except Exception as e:
+                print(f"Error occurred: {e}")
+
+    time.sleep(1.5)   
+
+if __name__=="__main__":
+  main()
---
+++ kafka_producer.py
@@ -0,0 +1,190 @@
+import json
+from kafka import KafkaProducer
+import os
+import time
+import uuid
+import random
+from datetime import timedelta, datetime
+from dotenv import find_dotenv, load_dotenv
+
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+
+# Kafka Environment variables
+KAFKA_BOOTSTRAP_SERVER = os.getenv("KAFKA_BOOTSTRAP_SERVER")
+VEHICLE_TOPIC = os.getenv("VEHICLE_TOPIC")
+GPS_TOPIC = os.getenv("GPS_TOPIC")
+TRAFFIC_TOPIC = os.getenv("TRAFFIC_TOPIC")
+WEATHER_TOPIC = os.getenv("WEATHER_TOPIC")
+EMERGENCY_TOPIC = os.getenv("EMERGENCY_TOPIC")
+
+# Coordinates of london
+LONDON_COORDINATES = {
+    'latitude': 51.5074,
+    'longitude': -0.1278
+}
+
+# Coordinates of Birmingham
+BIRMINGHAM_COORDINATES = {
+    "latitude": 52.4862,
+    "longitude": -1.8904
+}
+
+
+# Calculate the movement increments
+LATITUDE_INCREMENT = (BIRMINGHAM_COORDINATES["latitude"]
+                    - LONDON_COORDINATES["latitude"])/100
+LONGITUDE_INCREMENT = (BIRMINGHAM_COORDINATES["longitude"]
+                    - LONDON_COORDINATES["longitude"])/100
+
+
+random.seed(42)
+start_time = datetime.now()   # Vehicle Start time
+start_location = LONDON_COORDINATES.copy()  # Vehicle Start Location
+
+# Data Serializer before send the data into Kafka topics
+def json_serializer(data):
+    return json.dumps(data).encode("utf-8")
+
+# Generating the time
+def get_next_time():
+    global start_time
+
+    start_time += timedelta(seconds=random.randint(30,60))
+    return start_time
+
+# Generating the GPS data
+def generate_gps_data(device_id, timestamp, vehicle_type = "private"):
+    return {
+        "id": str(uuid.uuid4()),
+        "deviceID": device_id,
+        "timestamp": timestamp,
+        "speed": random.uniform(0,40),
+        "direction": "North-Eash",
+        "Vehicle_type": vehicle_type
+    }
+
+# Generating the Traffic Camera Data
+def generate_traffic_camera_data(device_id, timestamp, location, camera_id):
+    return {
+        "id": str(uuid.uuid4()),
+        "deviceID": device_id,
+        "cameraID": camera_id,
+        "location": location,
+        "timestamp": timestamp,
+        "snapshot": "Base64EncodedString"
+    }
+
+# Generating the Weather Data
+def generate_weather_data(device_id, timestamp, location):
+    return {
+        "id":str(uuid.uuid4()),
+        "deviceID": device_id,
+        "location": location,
+        "timestamp": timestamp,
+        "temperature": random.uniform(-5,27),
+        "weatherCondition": random.choice(['Sunny', 'Cloudy', 'Rain', 'Snow']),
+        "precipitation": random.uniform(0,25),
+        "windSpeed": random.uniform(0,100),
+        "humidity": random.randint(0,100),
+        "airQualityIndex": random.uniform(0,500)
+    }
+
+# Generating the emergency data
+def generate_emergency_incident_data(device_id, timestamp, location):
+    return {
+        "id": str(uuid.uuid4()),
+        "deviceID": device_id,
+        "incidentID": str(uuid.uuid4()),
+        "type": random.choice(["Accident", "Fire", "Medical", "Police", "None"]),
+        "timestamp": timestamp,
+        "location": location,
+        "status": random.choice(["Active", "Resolved"]),
+        "description": "Deacription of the incident"
+    }
+
+
+# Finding the location of vehicle
+def stimulate_vehicle_movement():
+    global start_location
+
+    # Move towards birminghm
+    start_location["latitude"] += LATITUDE_INCREMENT
+    start_location["longitude"] += LONGITUDE_INCREMENT
+
+    # Adding some randomness
+    start_location["latitude"] += random.uniform(-0.0005, 0.0005)
+    start_location["longitude"] += random.uniform(-0.0005, 0.0005)
+
+    return start_location
+
+# Generating vehicle data
+def generate_vehicle_data(device_id):
+    location = stimulate_vehicle_movement()
+    return {
+        "id": str(uuid.uuid4()),
+        "device": device_id,
+        "timestamp": get_next_time().isoformat(),
+        "location": (location['latitude'], location['longitude']),
+        "speed": random.uniform(10,40),
+        "direction": "North-East",
+        "make": "BMW",
+        "model": "C500",
+        "year": 2024,
+        "fuelType": "Hybrid"
+    }
+
+# Funciton to send the data into kafka topic
+def producer_data_to_kafka_topic(producer, topic, data):
+    producer.send(
+        topic,
+        data
+    )
+
+def simulate_journey(producer, device_id):
+    while True:
+        vehicle_data = generate_vehicle_data(device_id)
+        gps_data = generate_gps_data(device_id, vehicle_data["timestamp"])
+        traffic_camera_data = generate_traffic_camera_data(device_id, vehicle_data["timestamp"], vehicle_data["location"], "Canon_34312")
+        weather_data = generate_weather_data(device_id, vehicle_data["timestamp"], vehicle_data["location"])
+        emergency_incident_data = generate_emergency_incident_data(device_id, vehicle_data["timestamp"], vehicle_data["location"])
+
+        # Checking if the vehicle reached to its destination or not.
+        if (vehicle_data["location"][0] >= BIRMINGHAM_COORDINATES["latitude"]
+                and vehicle_data["location"][1] <= BIRMINGHAM_COORDINATES["longitude"]):
+                print("Vehicle reached Birminghm. Simulation ending.....")
+                break
+        
+        # Producing the data into kafka topic
+        producer_data_to_kafka_topic(producer, VEHICLE_TOPIC, vehicle_data)
+        producer_data_to_kafka_topic(producer, GPS_TOPIC, gps_data)
+        producer_data_to_kafka_topic(producer, TRAFFIC_TOPIC, traffic_camera_data)
+        producer_data_to_kafka_topic(producer, WEATHER_TOPIC, weather_data)
+        producer_data_to_kafka_topic(producer, EMERGENCY_TOPIC, emergency_incident_data)
+
+
+        print("vehicle_data", vehicle_data)
+        print()
+        print("gps_data", gps_data)
+        print()
+        print("traffic_camera_data", traffic_camera_data)
+        print()
+        print("weather_data", weather_data)
+        print()
+        print("emergency_incident_data", emergency_incident_data)
+        print()
+        
+        time.sleep(2)
+
+if __name__ == "__main__":
+    # Configuring the kafka producer
+    producer = KafkaProducer(
+        bootstrap_servers=['127.0.0.1:9092'],
+        value_serializer=json_serializer
+    )
+    try:
+        simulate_journey(producer, "Vehicle_123")
+    except KeyboardInterrupt:
+        print("Simulation ended by the user.")
+    except Exception as e:
+        print(f"Unexpected error occured {e}")
